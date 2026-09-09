ff8c14dec8f2ff0aa42d1998fa06cfbb6eb40305
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2026-09-09 10:34:03 -0700

    User post

--- packages/express-backend/src/index.ts
+++ packages/express-backend/src/index.ts
@@ -37,6 +37,17 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
+const addUser = (user: typeof users.users_list[0]) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
